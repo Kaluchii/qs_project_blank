ba046f8ca8f9dfb0641d8fbb4a81052149cd815f
Kaluchii <mr.kaluchii@mail.ru>
2019-04-23 23:13:55 +0600

    Changes

---
+++ resources/views/back/tps/last_modified.blade.php
@@ -0,0 +1,4 @@
+@foreach($blocks as $item)
+    <input class="form-control string" type="hidden" value="{{time()}}"
+           data-name="last_modified" data-type="string" data-block="{{$item}}" data-id="0">
+@endforeach
